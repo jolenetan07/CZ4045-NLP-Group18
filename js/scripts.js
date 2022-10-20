5164d64fda95e00c132baa34c853d1a72031035c
jolenetan07 <jolenetan07@gmail.com>
2022-10-21 05:13:07 +0800

    remove unused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-// SIDEBAR TOGGLE
-
-var sidebarOpen = false;
-var sidebar = document.getElementById("sidebar");
-
-function openSidebar() {
-  if(!sidebarOpen) {
-    sidebar.classList.add("sidebar-responsive");
-    sidebarOpen = true;
-  }
-}
-
-function closeSidebar() {
-  if(sidebarOpen) {
-    sidebar.classList.remove("sidebar-responsive");
-    sidebarOpen = false;
-  }
-}
-
-
-
 // ---------- CHARTS ----------
 
 // ----- Model 1 -----
