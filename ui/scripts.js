09d5e4d8263521e3ef309382c5e9170649730697
jolenetan07 <jolenetan07@gmail.com>
2022-11-05 02:52:13 +0800

    update deep learning results

--- ui/scripts.js
+++ ui/scripts.js
@@ -390,3 +390,235 @@ responsive: [{
 
 var pieChart6 = new ApexCharts(document.querySelector("#pie-chart-6"), pieChart6Options);
 pieChart6.render();
+
+// ----- lstm -----
+// Model 7 Bar
+var barChart7Options = {
+  series: [{
+    data: [5000, 3676, 8955]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Positive", "Neutral", "Negative"],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
+
+var barChart7 = new ApexCharts(document.querySelector("#bar-chart-7"), barChart7Options);
+barChart7.render();
+
+
+// Model 7 Pie
+var pieChart7Options = {
+  series: [5000, 3676, 8955],
+  chart: {
+  width: 400,
+  offsetX: 25,
+  offsetY: 40,
+  type: 'pie',
+},
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+labels: ['Positive', 'Neutral', 'Negative'],
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var pieChart7 = new ApexCharts(document.querySelector("#pie-chart-7"), pieChart7Options);
+pieChart7.render();
+
+// ----- dan -----
+// Model 8 Bar
+var barChart8Options = {
+  series: [{
+    data: [7700, 6122, 3809]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Positive", "Neutral", "Negative"],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
+
+var barChart8 = new ApexCharts(document.querySelector("#bar-chart-8"), barChart8Options);
+barChart8.render();
+
+
+// Model 8 Pie
+var pieChart8Options = {
+  series: [7700, 6122, 3809],
+  chart: {
+  width: 400,
+  offsetX: 25,
+  offsetY: 40,
+  type: 'pie',
+},
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+labels: ['Positive', 'Neutral', 'Negative'],
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var pieChart8 = new ApexCharts(document.querySelector("#pie-chart-8"), pieChart8Options);
+pieChart8.render();
+
+
+// ----- roberta-dan -----
+// Model 9 Bar
+var barChart9Options = {
+  series: [{
+    data: [4777, 3787, 9067]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Positive", "Neutral", "Negative"],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
+
+var barChart9 = new ApexCharts(document.querySelector("#bar-chart-9"), barChart9Options);
+barChart9.render();
+
+
+// Model 9 Pie
+var pieChart9Options = {
+  series: [4777, 3787, 9067],
+  chart: {
+  width: 400,
+  offsetX: 25,
+  offsetY: 40,
+  type: 'pie',
+},
+colors: [
+  "#367952",
+  "#f5b74f",
+  "#cc3c43",
+],
+labels: ['Positive', 'Neutral', 'Negative'],
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var pieChart9 = new ApexCharts(document.querySelector("#pie-chart-9"), pieChart9Options);
+pieChart9.render();
